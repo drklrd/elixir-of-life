e053632338cf8abb4fe78b743c668dcb69277a35
drklrd <srvbhattarai@gmail.com>
2017-04-07 07:49:06 +0545

    add tail recursion code

--- body_recursion.ex
+++ body_recursion.ex
@@ -1,9 +1,6 @@
 defmodule Sample.Enum do
-
-	def other_length([_ | tail]), do: other_length(tail,1)
-	def other_length([], len), do: len
-	def other_length([_ | tail],len), do: other_length(tail,len+1)
-	
+	def length([]), do: 0
+	def length([_ | tail]), do: 1 + length(tail)
 end
 
 # iex(1)> Sample.Enum.map([1,2,3],&(&1 *2))
